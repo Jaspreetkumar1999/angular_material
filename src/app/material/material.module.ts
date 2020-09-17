d9b426888ffb0d94a31a40ce2c23f7d6cb4e82a4
Jaspreet_Kumar <jaspreet@gsbitlabs.com.au>
2020-09-17 13:05:38 +0530

    aa

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
 
 const MaterialComponents = [
-  MatButtonModule
+  MatButtonModule,
+  MatIconModule,
+  MatBadgeModule
 ]
 
 @NgModule({
